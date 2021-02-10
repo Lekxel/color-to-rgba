0b7a97b2ae96909af9b2cd3713ff9d62a470761a
Lekxel <sirlekxel@gmail.com>
2021-02-10 20:37:51 +0100

    🚀  First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Colors to RGBA
+
+### This converts your any valid color format to rgba
+
+#### Example
+
+`import colorsToRGBA from 'colors-to-rgba`
+
+Then you can use like this:
+`let rgba = colorsToRGBA('red', 0.5)`
+`let rgba = colorsToRGBA('#f04', 0.8)`
+`let rgba = colorsToRGBA('#ff00f2')`
+
+### To get the list of common colors string
+
+`import {_colorsByName} from 'colors-to-rgba`
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import _colorsByName from './helpers/_colorsByName.js';
+
+/**
+ * @name colorsToRGBA
+ * @param color `string` The valid color to convert to rgba (e.g red or #ff0000)
+ * @param alpha `Number` The opacity of the generated rgba (default 1)
+ * @returns `String` It returns rgba with the alpha set the specified value
+ */
+const colorsToRGBA = (color: string, alpha: number) => {
+  return /^#([A-Fa-f0-9]{3}){1,2}$/.test(color) ? convert(color, alpha) : convert(_colorsByName[color], alpha);
+};
+
+const convert = (color: string, alpha: number = 1) => {
+  let eachChar: string[];
+  eachChar = color.substring(1).split('');
+  if (eachChar.length === 3) {
+    eachChar = [eachChar[0], eachChar[0], eachChar[1], eachChar[1], eachChar[2], eachChar[2]];
+  }
+
+  let colorOctal: number = Number('0x' + eachChar.join(''));
+
+  return `rgba(${[(colorOctal >> 16) & 255, (colorOctal >> 8) & 255, colorOctal & 255].join(',')},${alpha})`;
+};
+
+export { _colorsByName };
+
+export default colorsToRGBA;
